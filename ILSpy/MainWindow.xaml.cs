4a3e9bc6096c3b7809f04d7a4b9dfc5b0f91c215
Ilyas <astarion@protonmail.com>
2020-05-11 23:43:25 +0600

    Loading assembly from file

--- ILSpy/MainWindow.xaml.cs
+++ ILSpy/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,95 @@ using System.Windows.Shapes;
 
 namespace ILSpy
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    internal enum TreeViewItemType
+    {
+        Assembly,
+        References,
+        Reference
+    }
+
     public partial class MainWindow : Window
     {
+        private string FileName { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ExitMenu_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+        private void OpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog Dialog = new Microsoft.Win32.OpenFileDialog();
+            Dialog.DefaultExt = ".exe";
+            Dialog.Filter = "Execution Files (*.exe)|*.exe|Dynamic Link Libraries (*.dll)|*.dll";
+
+            Nullable<bool> result = Dialog.ShowDialog();
+
+            if (result == true)
+            {
+                this.FileName = Dialog.FileName;
+                LoadReflection();
+            }
+        }
+
+        private TreeViewItem ConfigureTreeViewItemSource(string Name, TreeViewItemType Type)
+        {
+            TreeViewItem Item = new TreeViewItem();
+            StackPanel Panel = new StackPanel();
+            Image Image = new Image();
+            TextBlock Block = new TextBlock();
+
+            if(Type == TreeViewItemType.Assembly)
+            {
+                Image.Source = new BitmapImage(new Uri("../../Images/CSAssemblyInfoFile_16x.png", UriKind.Relative));
+            }
+            else
+            {
+                Image.Source = new BitmapImage(new Uri("../../Images/Reference_16x.png", UriKind.Relative));
+            }
+            Image.Margin = new Thickness(5, 0, 5, 0);
+            Block.Text = Name;
+
+            Panel.Children.Add(Image);
+            Panel.Children.Add(Block);
+
+            Item.Header = Panel;
+
+            return Item;
+        }
+
+        private void LoadReflection()
+        {
+            try
+            {
+                /*Assembly FileAssembly = Assembly.LoadFrom(this.FileName);
+                TreeViewItem MainItem = ConfigureTreeViewItemSource(FileAssembly.GetName().Name, TreeViewItemType.Assembly);
+                TreeViewItem References = ConfigureTreeViewItemSource("References", TreeViewItemType.References);
+
+                MainItem.Items.Add(References);
+
+                foreach(AssemblyName Name in FileAssembly.GetReferencedAssemblies())
+                {
+                    //Assembly InnerAssembly = Assembly.Load(Name);
+                    References.Items.Add(ConfigureTreeViewItemSource(Name.Name, TreeViewItemType.Reference));
+                }
+
+                this.AssemblyInfoTree.Items.Add(MainItem);*/
+            }
+            catch(System.IO.IOException ex)
+            {
+                MessageBox.Show($"An IO error occurred during opening this file: {ex.Message}");
+                return;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"An unkown error occurred during opening this file: {ex.Message}");
+                return;
+            }
         }
     }
 }
